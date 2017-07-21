96f366435d28f784bbc72260d2752f02bf871f52
James Reed <james@jamesreed.name>
2017-07-21 21:13:51 +0100

    bot starts and joins

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,74 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TypeOperators, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE DataKinds, TypeFamilies #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
-import Data.Maybe
+import Control.Monad (mzero)
+import Data.Proxy
+import GHC.TypeLits
 
-import Network.URL
-import Pipes.Core
+import Network.Discord
 
-import Network.Discord.Types
-import Network.Discord.Gateway
-import Network.Discord.Rest
+import Control.Monad.IO.Class
+import Data.Text
 
-data LogClient = LClient
-instance Client LogClient where
-  getAuth _ = Bot "MzM4MDE2ODIyNTU5NTA2NDMz.DFPSQQ.sHzd9kjiUpQJ-yEyXVBMPuJU0YM"
+instance DiscordAuth IO where
+  auth    = return $ Bot "MzM4MDE2ODIyNTU5NTA2NDMz.DFPSQQ.sHzd9kjiUpQJ-yEyXVBMPuJU0YM"
+  version = return "0.4.2"
+  runIO   = id
+
+data Command (a :: Symbol)
+
+instance KnownSymbol a => EventMap (Command a) (DiscordApp IO) where
+  type Domain   (Command a) = Message
+  type Codomain (Command a) = Message
+
+  mapEvent p (m@Message{messageContent = c,messageAuthor = User{userIsBot = bot}})
+    | bot = do
+      liftIO $ putStrLn "Ignoring Bot Message"
+      mzero
+    | unpack c == symbolVal (commandName p) = do
+      liftIO $ putStrLn "Command Match"
+      return m
+    | otherwise = do
+      liftIO . putStrLn $ "No Command Match: " ++ show c ++ "!=" ++ symbolVal (commandName p)
+      mzero
+    where
+      commandName :: Proxy (Command a) -> Proxy a
+      commandName _ = Proxy
+
+data Reply (a :: Symbol)
+
+instance KnownSymbol a => EventMap (Reply a) (DiscordApp IO) where
+  type Domain   (Reply a) = Message
+  type Codomain (Reply a) = ()
+
+  mapEvent p (Message{messageChannel = c}) = do
+    _ <- doFetch $ CreateMessage c (pack . symbolVal $ replyText p) Nothing
+    return ()
+    where
+      replyText :: Proxy (Reply a) -> Proxy a
+      replyText _ = Proxy
+
+data LogEvent a
+
+instance Show a => EventMap (LogEvent a) (DiscordApp IO) where
+  type Domain   (LogEvent a) = a
+  type Codomain (LogEvent a) = ()
+
+  mapEvent _ e = do
+    liftIO $ putStrLn "Logging Event"
+    liftIO . putStrLn $ show e
+
+type PingPongApp = 
+  (
+    (MessageCreateEvent :<>: MessageUpdateEvent) :> 
+      (    (Command "ping" :> Reply "pong")
+      :<>: (Command "pong" :> Reply "ping")
+      :<>: (Command "flim" :> Reply "flam")
+      )
+  ) :<>: LogEvent Event
+
+instance EventHandler PingPongApp IO
 
 main :: IO ()
-main = runWebsocket (fromJust $ importURL "wss://gateway.discord.gg") LClient $ do
-  fetch' (CreateMessage 188134500411244545 "Hello, World!" Nothing)
-  fetch' (CreateMessage 188134500411244545 "I'm running discord.hs!" Nothing)
+main = runBot (Proxy :: Proxy (IO PingPongApp))
